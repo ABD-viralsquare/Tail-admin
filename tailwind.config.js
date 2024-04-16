6b2a7c3566b53099013b8694acf68dd65cab0413
abd <abdullah.dokota@gmail.com>
2024-04-16 15:03:00 +0500

    In this site i learned how to make responsive site on large scale

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        meta3:"#10B981",
+        greeen:"#10B981",
+        graywla:"#64748B",
+        bgwlacolor:"#F1F5F9",
+        bgiconwla:"#3C50E0",
+        headingwlacolor:"#1C2434",
+        rarewla:"#259AE6",
+        abcd:"#64748B",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
